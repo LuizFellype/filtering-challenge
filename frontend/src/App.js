ecd0f85d8eae3c886a5576b74f9991ee8b191248
Luiz Fellype <lfcassago@gmail.com>
2025-06-16 13:12:03 -0300

    fix: improve layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-3xl font-bold mb-8 px-4 text-center">Recomendador de Produtos RD Station</h1>
       <div className="mb-4 flex justify-center">
-        <p className="text-lg px-4 w-full md:w-2/3 text-justify">
+        <p className="text-lg px-4 w-full md:w-5/6 text-justify">
           Bem-vindo ao Recomendador de Produtos RD Station. Aqui você pode encontrar uma variedade de produtos da RD Station, cada um projetado para atender às necessidades específicas do seu negócio. De CRM a Marketing, de Conversas a Inteligência Artificial, temos uma solução para ajudar você a alcançar seus objetivos. Use o formulário abaixo para selecionar suas preferências e funcionalidades desejadas e receba recomendações personalizadas de produtos que melhor atendam às suas necessidades.
         </p>
       </div>
--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -11,36 +11,41 @@ function Form({ onSubmit, preferences, features }) {
     selectedFeatures: [],
     selectedRecommendationType: '',
   });
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    
+
     onSubmit(formData)
   };
 
   return (
     <form
-      className="max-w-md mx-auto p-4 bg-white rounded-lg shadow-md"
+      className="max-w-3xl mx-auto p-4 bg-white rounded-lg shadow-md"
       onSubmit={handleSubmit}
     >
-      <Preferences
-        preferences={preferences}
-        onPreferenceChange={(selected) =>
-          handleChange('selectedPreferences', selected)
-        }
-      />
-      <Features
-        features={features}
-        onFeatureChange={(selected) =>
-          handleChange('selectedFeatures', selected)
-        }
-      />
-      <RecommendationType
-        onRecommendationTypeChange={(selected) =>
-          handleChange('selectedRecommendationType', selected)
-        }
-      />
-      <SubmitButton text="Obter recomendação" />
+      <div className='md:flex md:justify-between md:gap-4'>
+        <Preferences
+          preferences={preferences}
+          onPreferenceChange={(selected) =>
+            handleChange('selectedPreferences', selected)
+          }
+        />
+        <Features
+          features={features}
+          onFeatureChange={(selected) =>
+            handleChange('selectedFeatures', selected)
+          }
+        />
+      </div>
+
+      <div className='flex flex-col md:flex-row md:justify-between items-center md:gap-4'>
+        <RecommendationType
+          onRecommendationTypeChange={(selected) =>
+            handleChange('selectedRecommendationType', selected)
+          }
+        />
+        <SubmitButton text="Obter recomendação" />
+      </div>
     </form>
   );
 }
